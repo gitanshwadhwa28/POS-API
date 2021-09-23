b5b3c0c76df04908b0ae28b097a5037e6e3a2f87
gitanshwadhwa28 <gitanshwadhwa0028@gmail.com>
2021-09-24 02:39:49 +0530

    create account

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,19 +1,21 @@
 const Web3 = require('web3')
+const provider = new Web3.providers.HttpProvider(
+    "https://rinkeby.infura.io/v3/33a046f29f2d4dbf966f5e2a548e576c"
+);
+var web3 = new Web3(Web3.currentProvider || provider);
 
-let web3;
-
-if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
-    // We are in the browser and metamask is running.
-    window.ethereum.request({ method: "eth_requestAccounts" });
-    web3 = new Web3(window.ethereum);
-    console.log("metamask connected")
-} else {
-    // We are on the server *OR* the user is not running metamask
-    const provider = new Web3.providers.HttpProvider(
-        "https://rinkeby.infura.io/v3/33a046f29f2d4dbf966f5e2a548e576c"
-    );
-    web3 = new Web3(provider);
-    console.log("metamask not connected")
-}
+// if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+//     // We are in the browser and metamask is running.
+//     window.ethereum.request({ method: "eth_requestAccounts" });
+//     web3 = new Web3(window.ethereum);
+//     console.log("metamask connected")
+// } else {
+//     // We are on the server *OR* the user is not running metamask
+//     const provider = new Web3.providers.HttpProvider(
+//         "https://rinkeby.infura.io/v3/33a046f29f2d4dbf966f5e2a548e576c"
+//     );
+//     web3 = new Web3(provider);
+//     console.log("metamask not connected")
+// }
 
 module.exports = web3
--- src/public/js/eth.js
+++ src/public/js/eth.js
@@ -1,12 +1,52 @@
-const web3 = require('../../../ethereum/web3')
 
-function init() {
-    console.log("hah")
-    var btn = document.getElementById("btn");
-    btn.addEventListener("click", async () => {
-        document.getElementById('demo').innerHTML = Date()
-        const accounts = await web3.eth.getAccounts()
-        await factory.methods.createCampaign(this.state.minimum).send({ from: accounts[0] })
+let web3;
+
+if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+    window.ethereum.request({ method: "eth_requestAccounts" });
+    web3 = new Web3(window.ethereum);
+    console.log("metamask connected")
+} else {
+    const provider = new Web3.providers.HttpProvider(
+        "https://rinkeby.infura.io/v3/33a046f29f2d4dbf966f5e2a548e57"
+    );
+    web3 = new Web3(provider);
+    console.log("metamask not connected")
+}
+
+var factory = JSON.parse('{\"abi\":[{\"inputs\":[],\"name\":\"createAccount\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"name\":\"deployedAccounts\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"getDeployedAccounts\",\"outputs\":[{\"internalType\":\"address[]\",\"name\":\"\",\"type\":\"address[]\"}],\"stateMutability\":\"view\",\"type\":\"function\"}]}');
+console.log(factory.abi)
+let contract;
+
+function myFunction2() {
+    contract = new web3.eth.Contract(
+        factory.abi,
+        '0xC7Cd8F16D7ff47586045187Eb02038F14d3853cD'
+    )
+    console.log(contract)
+}
+
+myFunction2()
+
+let account;
+
+web3.eth.getAccounts(function (err, accounts) {
+    if (err != null) {
+        alert("Error retrieving accounts.");
+        return;
+    }
+    if (accounts.length == 0) {
+        alert("No account found");
+        return;
+    }
+    account = accounts[0];
+    console.log(account)
+    web3.eth.defaultAccount = account;
+});
+
+
+
+function myFunction() {
+    contract.methods.createAccount().send({ from: account }).then(function (tx) {
+        console.log("Transaction: ", tx);
     });
 }
-window.onload = init;
