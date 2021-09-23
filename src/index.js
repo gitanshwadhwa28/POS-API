2b803bad208327a32a04b3ce2de89ffb4ba79be9
gitanshwadhwa28 <gitanshwadhwa0028@gmail.com>
2021-09-23 15:26:56 +0530

    get accounts

--- src/index.js
+++ src/index.js
@@ -36,21 +36,7 @@ app.get('/accounts', async (req, res) => {
 app.get('/create', async (req, res) => {
     try {
 
-        let web3;
-
-        if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
-            window.ethereum.request({ method: "eth_requestAccounts" });
-            web3 = new Web3(window.ethereum);
-            console.log("metamask connect")
-        } else {
-            const provider = new Web3.providers.HttpProvider(
-                "https://rinkeby.infura.io/v3/33a046f29f2d4dbf966f5e2a548e576c"
-            );
-            web3 = new Web3(provider);
-            console.log("metamask not connected")
-        }
-
-        const accounts = await web3.eth.getAccounts()
+        const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
         res.status(202).send(accounts)
         // await instance.methods.createAccount().send({ from: accounts[0] })
         // res.send('Contract deployed')
