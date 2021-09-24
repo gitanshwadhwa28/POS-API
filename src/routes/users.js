57decad0b6ffac7fb50058ccb60528e13482605a
Arnav Mahajan <arnav.mahajan.2001@gmail.com>
2021-09-24 17:26:56 +0530

    Added login and signup

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,8 +5,14 @@ const { forwardAuthenticated, ensureAuthenticated } = require('../config/auth');
 
 const User = require('../models/user');
 
+router.get('/signup', forwardAuthenticated, function(req, res){
+    res.render("register");
+});
 router.post('/signup',forwardAuthenticated, userController.signup);
 
+router.get('/signin',forwardAuthenticated, function(req, res){
+    res.render("login");
+});
 router.post('/signin', forwardAuthenticated, userController.signin);
 
 router.post("/forgetPassword", userController.forgetPassword);
