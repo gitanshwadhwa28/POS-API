8c71f5ddaa8e63615238748112802792223a4cfc
gitanshwadhwa28 <gitanshwadhwa0028@gmail.com>
2021-09-25 00:20:08 +0530

    signup2

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,44 +8,7 @@ const User = require('../models/user');
 router.get('/signup', forwardAuthenticated, function (req, res) {
     res.render("register");
 });
-router.post('/signup', forwardAuthenticated, async (req, res) => {
-    const { name, email, password, password2 } = req.body;
-    let errors = [];
-
-    console.log(req.body.name)
-
-    if (!name || !email || !password || !password2) {
-        errors.push({ msg: 'Please enter all fields' });
-    }
-
-    if (password != password2) {
-        errors.push({ msg: 'Passwords do not match' });
-    }
-
-    if (password.length < 3) {
-        errors.push({ msg: 'Password must be at least 3 characters' });
-    }
-
-
-    const userExists = await User.findOne({ email: req.body.email });
-    if (userExists) return res.status(400).send('Email already exist');
-
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(req.body.password, salt);
-
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: hashPassword
-    })
-
-    try {
-        const savedUser = await user.save();
-        res.redirect("signin");
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
+router.post('/signup', forwardAuthenticated, userController.signup);
 
 router.get('/signin', forwardAuthenticated, function (req, res) {
     res.render("login");
